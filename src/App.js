26a3633d340e33fdeea53b379a4cb7e88a4c5b92
Francois ROLAND <francois.roland@iepscf-uccle.be>
2022-05-24 09:26:39 +0000

    Design section list.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
-import { Container, Stack } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Fab,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import MenuBar from "./components/MenuBar";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
   return (
     <Stack>
       <MenuBar />
       <Container maxWidth="md">
-        <p>Hello world!</p>
+        <Stack spacing={2}>
+          <h2>Sections</h2>
+          <Grid container spacing={2}>
+            <Grid item sm={6} md={4}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Informatique de gestion
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item sm={6} md={4}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Informatique de gestion
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item sm={6} md={4}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Informatique de gestion
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item sm={6} md={4}>
+              <Card>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    Informatique de gestion
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+          <Fab color="primary" aria-label="add">
+            <AddIcon />
+          </Fab>
+        </Stack>
       </Container>
     </Stack>
   );
