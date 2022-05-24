07151e3cde8effef44f395f46cef0c7f9b4f4107
Francois ROLAND <francois.roland@iepscf-uccle.be>
2022-05-24 10:26:42 +0000

    Fetch data from backend.

--- src/App.js
+++ src/App.js
@@ -1,66 +1,13 @@
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Container,
-  Fab,
-  Grid,
-  Stack,
-  Typography,
-} from "@mui/material";
+import { Container, Stack } from "@mui/material";
 import MenuBar from "./components/MenuBar";
-import AddIcon from "@mui/icons-material/Add";
+import SectionList from "./pages/SectionList";
 
 function App() {
   return (
     <Stack>
       <MenuBar />
       <Container maxWidth="md">
-        <Stack spacing={2}>
-          <h2>Sections</h2>
-          <Grid container spacing={2}>
-            <Grid item sm={6} md={4}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    Informatique de gestion
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item sm={6} md={4}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    Informatique de gestion
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item sm={6} md={4}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    Informatique de gestion
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item sm={6} md={4}>
-              <Card>
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    Informatique de gestion
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          </Grid>
-          <Fab color="primary" aria-label="add">
-            <AddIcon />
-          </Fab>
-        </Stack>
+        <SectionList />
       </Container>
     </Stack>
   );
---
+++ src/pages/SectionList.js
@@ -0,0 +1,57 @@
+import {
+  Alert,
+  Card,
+  CardContent,
+  CircularProgress,
+  Fab,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import useAxios from "axios-hooks";
+
+const SectionList = () => {
+  const [{ data, loading, error }] = useAxios(
+    "http://localhost:8080/api/v1/sections"
+  );
+  return (
+    <Stack spacing={2}>
+      <h2>Sections</h2>
+      {loading && <CircularProgress />}
+      {error && (
+        <Alert severity="error">
+          Le chargement ne s'est pas déroulé comme prévu.
+        </Alert>
+      )}
+      {data && (
+        <>
+          <Grid container spacing={2}>
+            {data.map((section) => (
+              <SectionListItem section={section} />
+            ))}
+          </Grid>
+          <Fab color="primary" aria-label="add">
+            <AddIcon />
+          </Fab>
+        </>
+      )}
+    </Stack>
+  );
+};
+
+const SectionListItem = ({ section }) => {
+  return (
+    <Grid item sm={6} md={4} key={section.id}>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {section.name}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+export default SectionList;
