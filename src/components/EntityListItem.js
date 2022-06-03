ef1e58fb44d306cfa4339cfa6ee902eeb50f3e85
Francois ROLAND <froland@froland.be>
2022-06-03 10:22:35 +0000

    Receive page of courses.

--- src/components/EntityListItem.js
+++ src/components/EntityListItem.js
@@ -2,7 +2,7 @@ import { Card, CardContent, Grid, Typography } from "@mui/material";
 
 const EntityListItem = ({ entity }) => {
   return (
-    <Grid item sm={6} md={4}>
+    <Grid item sm={12} md={6}>
       <Card>
         <CardContent>
           <Typography variant="h5" component="div">
--- src/pages/CourseList.js
+++ src/pages/CourseList.js
@@ -1,9 +1,20 @@
+import { Grid, Pagination } from "@mui/material";
 import useAxios from "axios-hooks";
+import React from "react";
 import EntityList from "../components/EntityList";
 import EntityListItem from "../components/EntityListItem";
 
 const CourseList = () => {
-  const [{ data, loading, error }] = useAxios("/courses");
+  const [page, setPage] = React.useState(1);
+  const [{ data, loading, error }] = useAxios({
+    url: "/courses",
+    params: { page: page - 1, size: 10 },
+  });
+
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
   return (
     <EntityList
       title="Cours"
@@ -12,9 +23,19 @@ const CourseList = () => {
       error={error}
     >
       {data &&
-        data.map((course) => (
+        data.content.map((course) => (
           <EntityListItem entity={course} key={course.id} />
         ))}
+
+      {data && (
+        <Grid item xs={12}>
+          <Pagination
+            count={data.totalPages}
+            onChange={handlePageChange}
+            page={page}
+          />
+        </Grid>
+      )}
     </EntityList>
   );
 };
